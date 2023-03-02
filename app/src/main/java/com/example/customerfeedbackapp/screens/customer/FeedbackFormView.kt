6f47ea2c9f269f8a3a0a589a79010c35e1b34001
Juho Järvilehto <77263040+jarvilehto@users.noreply.github.com>
2023-03-02 18:53:16 +0200

    improved feedback

--- app/src/main/java/com/example/customerfeedbackapp/screens/customer/FeedbackFormView.kt
+++ app/src/main/java/com/example/customerfeedbackapp/screens/customer/FeedbackFormView.kt
@@ -1,36 +1,61 @@
 package com.example.customerfeedbackapp.screens.customer
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.customerfeedbackapp.models.Feedback
 
 @Composable
 fun FeedbackFormView(productViewModel: ProductViewModel) {
     var feedback by remember { mutableStateOf("") }
     var rating by remember { mutableStateOf(0) }
-    Column(modifier = Modifier.padding(16.dp)) {
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize()
+    ) {
+        Spacer(modifier = Modifier.height(30.dp))
         FeedbackHeader(productName = productViewModel.currentItem2.title ?: "")
-        TextField(
-            value = feedback, onValueChange = { feedback = it },
-            label = { Text(text = "Leave feedback here!") }, modifier =
-            Modifier
-                .height(200.dp)
-                .fillMaxWidth()
-        )
+        Spacer(modifier = Modifier.height(30.dp))
+        Card(
+            modifier = Modifier
+                .padding(top = 10.dp)
+                .heightIn(min = Dp.Unspecified, max = 120.dp),
+            shape = RoundedCornerShape(10.dp),
+            elevation = 5.dp,
+            border = BorderStroke(2.dp, MaterialTheme.colors.primary)
+        ) {
+            TextField(
+                value = feedback, onValueChange = { feedback = it },
+                label = { Text(text = "Leave feedback here!") }, modifier =
+                Modifier
+                    .height(200.dp)
+                    .background(Color.White)
+                    .fillMaxWidth(), colors = TextFieldDefaults
+                    .textFieldColors(backgroundColor = MaterialTheme.colors.onPrimary )
+            )
+        }
+        Spacer(modifier = Modifier.height(15.dp))
+        Text(text = "Rate the product:")
         Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
             for (i in 1..5) {
                 Button(onClick = { rating = i }) {
-                    Text("$i", fontSize = 24.sp)
+                    Text("$i", fontSize = 20.sp)
                 }
             }
         }
+        Spacer(modifier = Modifier.height(20.dp))
         Button(onClick = {
             if (feedback.isNotEmpty()) productViewModel.rate(feedback, rating)
         }, modifier = Modifier.fillMaxWidth()) {
@@ -40,6 +65,9 @@ fun FeedbackFormView(productViewModel: ProductViewModel) {
 
     }
 }
+
+
+
 
 @Composable
 fun FeedbackHeader(productName: String) {
