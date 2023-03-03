751f22b6dc62f6ef3030d167118f5a47dce04583
Juho Järvilehto <77263040+jarvilehto@users.noreply.github.com>
2023-03-03 15:49:19 +0200

    Added border to CameraView

--- app/src/main/java/com/example/customerfeedbackapp/screens/camera/CameraView.kt
+++ app/src/main/java/com/example/customerfeedbackapp/screens/camera/CameraView.kt
@@ -10,14 +10,16 @@ import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
@@ -52,50 +54,65 @@ fun CameraView(productViewModel: ProductViewModel, navController: NavController)
     LaunchedEffect(key1 = true) {
         launcher.launch(Manifest.permission.CAMERA)
     }
-    Column(modifier = Modifier.fillMaxHeight().fillMaxWidth()) {
-        if (hasCamPermission) {
-            AndroidView(
-                factory = { context ->
-                    val previewView = PreviewView(context)
-                    val preview = Preview.Builder().build()
-                    val selector = CameraSelector.Builder()
-                        .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-                        .build()
-                    preview.setSurfaceProvider(previewView.surfaceProvider)
-                    val imageAnalysis = ImageAnalysis.Builder()
-                        .setTargetResolution(
-                            Size(
-                                previewView.width,
-                                previewView.height
+    Box(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth()
+        ) {
+            if (hasCamPermission) {
+                AndroidView(
+                    factory = { context ->
+                        val previewView = PreviewView(context)
+                        val preview = Preview.Builder().build()
+                        val selector = CameraSelector.Builder()
+                            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                            .build()
+                        preview.setSurfaceProvider(previewView.surfaceProvider)
+                        val imageAnalysis = ImageAnalysis.Builder()
+                            .setTargetResolution(
+                                Size(
+                                    previewView.width,
+                                    previewView.height
+                                )
                             )
-                        )
-                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-                        .build()
-                    imageAnalysis.setAnalyzer(
-                        ContextCompat.getMainExecutor(context),
-                        QrAnalyzer { result ->
-                            code = result
-                            for(barcode in products){
-                                if(barcode.ean!!.contains(result)){
-                                    productViewModel.currentItem2 = barcode
-                                    navController.navigate("FeedbackFormView")
+                            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                            .build()
+                        imageAnalysis.setAnalyzer(
+                            ContextCompat.getMainExecutor(context),
+                            QrAnalyzer { result ->
+                                code = result
+                                for (barcode in products) {
+                                    if (barcode.ean!!.contains(result)) {
+                                        productViewModel.currentItem2 = barcode
+                                        navController.navigate("FeedbackFormView")
+                                    }
                                 }
                             }
-                        }
-                    )
-                    try {
-                        cameraProviderFuture.get().bindToLifecycle(
-                            lifecycleOwner,
-                            selector,
-                            preview,
-                            imageAnalysis
                         )
-                    } catch (e: Exception) {
-                        e.printStackTrace()
-                    }
-                    previewView
-                }, modifier = Modifier.fillMaxSize()
-            )
+                        try {
+                            cameraProviderFuture.get().bindToLifecycle(
+                                lifecycleOwner,
+                                selector,
+                                preview,
+                                imageAnalysis
+                            )
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        }
+                        previewView
+                    }, modifier = Modifier.fillMaxSize()
+                )
+            }
+        }
+        Column(Modifier.border(5.dp, Color.White).width(200.dp).height(500.dp),
+        ) {
+
         }
     }
 }
